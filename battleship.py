03901bb9794559dec42a45f9fe43c08be8001885
aisair <ais11air@gmail.com>
2020-02-03 11:07:21 -0600

    add .gitignore

--- battleship.py
+++ battleship.py
@@ -1,57 +1,105 @@
 # This is the in-class tutorial for a basic battle ship game. This game will have one ship, one unit long.  To get a
-# C, the game will need 1 ship 3 units long. to get a B, the game should have 3 ships  of different lengths,
+# C, the game will need 1 ship 3 units long. To get a B, the game should have 3 ships  of different lengths,
 # randomly placed vertically or horizontally on the board. To get an A, there must be a computer playing a person
 # player.
 
-# We will import the random library to place the 1 x 1 ship randomly
+from colorama import init, Fore
 import random
-
-# We will import the time library so we can add a delay between turns
 import time
 
-# begin by creating a the game-board. For this level it will be a 5x5 units and initially will be set to show all "O"'s
+init()  # initiate colorama for colorful text
 
-
+# begin by creating a the game-board. For ths level it will be a 5x5 units and initially will be set to show all "-"'s
+game_board = []
+number_turns = 12
 # append a row of 5 "-'s"
+for x in range(0, 5):
+    game_board.append(["-"] * 5)
 
 
 # function to print the board
+def print_board(board):
+    for row in board:
+        print(" ".join(row))
 
 
+print(
+    "Welcome to Battleship! There is one ship that is one unit long.\nThe board is a 5 x 5 grid. You will get"
+    , number_turns, "guesses to find the ship. Good luck!")
 
-# Start the game play . If  you want to add delays, so the game plays more naturally use time.sleep(seconds)
+print_board(game_board)
+
+# Start the game play. If  you want to add delays, so the game plays more naturally use time.sleep(seconds)
+time.sleep(1)
 
 
-# We need to randomly place the ship on the board.  The x-coordinate should have value between 0 and 4.
-# If this were a C, B or A level project you would also have to randomly choose vertical or horizontal
+# We need to randomly place the ship on the board. The x-coordinate should have value between 0 and 4. If this were a
+# C, B or A level project you would also have to randomly choose vertical or horizontal
+def random_row(board):
+    return random.randint(0, len(board) - 1)
 
 
+def random_column(board):
+    return random.randint(0, len(board[0]) - 1)
 
 
 # The C level will need an extra function and variable to keep track of vertical or horizontal for the 3 x 1 ship.
+ship_row = random_row(game_board)
+ship_column = random_column(game_board)
 
+# For Testing Where Is The Ship?
+print("ship row:", ship_row + 1)
+print("ship column:", ship_column + 1)
 
-# For the highe level projects, you will need to make sure the ships do not go off the board, and do not overlap.
+# For the higher level projects, you will need to make sure the ships do not go off the board, and do not overlap.
 
 # The next 2 lines are to check where the ship is being placed. so should be removed for final game
 
 
-# Let the player guess where the ship is
-# for the basic game we will let the player input integers, but for the real battleship game,
-# you will need to enter inputs as "C4" or "D5"
-# so the this row input should be a letter in the version you build.
+# Let the player guess where the ship is. For the basic game we will let the player input integers, but for the real
+# battleship game, you will need to enter inputs as "C4" or "D5". so the this row input should be a letter in the
+# version you build.
+for current_turn in range(number_turns):
+    print("You are on turn ", current_turn + 1, "of", number_turns)
+    guess = input("Input guess: ")
+    if len(guess) != 2:
+        print("The guess is not valid!")
+    else:
+        switch = {
+            "a": 0,
+            "b": 1,
+            "c": 2,
+            "d": 3,
+            "e": 4,
+        }
+        guess_row = switch.get(guess[0])
+        guess_column = int(guess[1]) - 1
 
+    # Check if the guess is a hit or a miss, or not on the board.
+    # When you build the game make sure to let them know if they already guessed that spot.
 
+    if guess_row == ship_row and guess_column == ship_column:
+        game_board[guess_row][guess_column] = Fore.RED + "X" + Fore.RESET
+        # This is a hit
+        print_board(game_board)
+        print("You sank my battleship!")
+        break
+    else:
+        if guess_row not in range(5) or \
+                guess_column not in range(5):
+            print("You missed the ocean!")
+        elif game_board[guess_row][guess_column] == "X":
+            print("You already guessed that location!")
+        else:
+            print("You missed my battleship!")
+            game_board[guess_row][guess_column] = "X"
+        if current_turn == number_turns - 1:
+            print("You have ran out of turns. The game is over!")
+            print("The ship was at row %d and column %d" % ship_row, ship_column)
+        print_board(game_board)
 
-
-# Check if the guess is a hit or a miss, or not on the board.
-# When you build the game make sure to let them know if they already guessed that spot.
-
-
-
-
-
-# Your print statement should should update how many ships are remaining and how many are sunk from your set of ships.
-
+        # Your print statement should should update how many ships are remaining and how many are sunk from your set of
+        # ships.
+        print("You have one ship remaining. It is a 1 x 1 unit.")
 
 # Convert Letters to numbers.
