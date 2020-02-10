d638d4e10e6077826d2f9244bbe70811b6476ba9
aisair <ais11air@gmail.com>
2020-02-10 01:15:48 -0600

    changed .gitignore also battleship.py doesn't work yet but making progress

--- battleship.py
+++ battleship.py
@@ -1,8 +1,3 @@
-# This is the in-class tutorial for a basic battle ship game. This game will have one ship, one unit long.  To get a
-# C, the game will need 1 ship 3 units long. To get a B, the game should have 3 ships  of different lengths,
-# randomly placed vertically or horizontally on the board. To get an A, there must be a computer playing a person
-# player.
-
 from prettytable import PrettyTable
 from colorama import init, Fore
 import random
@@ -10,13 +5,10 @@ import random
 init()  # initiate colorama for colorful text
 
 number_turns = 12
+number_hits = 0
 
 
 class Coordinate:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-
     x = 0
     y = 0
 
@@ -27,10 +19,6 @@ class Ship:
     coord_2 = Coordinate()
     length = 0
 
-    def __init__(self):
-        self.length = 0
-        self.orientation = 0
-
 
 # start with a 5 by 5 board filled with "O"s
 game_board = PrettyTable()
@@ -38,16 +26,24 @@ board_rows = 5
 board_columns = 5
 board_info = []
 ship = Ship
-lettertrans = {
+ship.length = 3
+letter_to_number = {
     "a": 0,
     "b": 1,
     "c": 2,
     "d": 3,
     "e": 4,
 }
+number_to_letter = {
+    0: "a",
+    1: "b",
+    2: "c",
+    3: "d",
+    4: "e",
+}
 
 
-def print_board(init_board=0):
+def print_board(init_board=0):  # change this to be less brute forcey (more methodical) & use python string library
     if init_board == 1:
         game_board.field_names = ["", "1", "2", "3", "4", "5"]
         i = 0
@@ -62,9 +58,9 @@ def print_board(init_board=0):
     game_board.clear_rows()
     game_board.add_row(["A", board_info[0][0], board_info[0][1], board_info[0][2], board_info[0][3], board_info[0][4]])
     game_board.add_row(["B", board_info[1][0], board_info[1][1], board_info[1][2], board_info[1][3], board_info[1][4]])
-    game_board.add_row(["C", board_info[2][0], board_info[0][1], board_info[2][2], board_info[2][3], board_info[2][4]])
-    game_board.add_row(["D", board_info[3][0], board_info[0][1], board_info[3][2], board_info[3][3], board_info[3][4]])
-    game_board.add_row(["E", board_info[4][0], board_info[0][1], board_info[4][2], board_info[4][3], board_info[4][4]])
+    game_board.add_row(["C", board_info[2][0], board_info[2][1], board_info[2][2], board_info[2][3], board_info[2][4]])
+    game_board.add_row(["D", board_info[3][0], board_info[3][1], board_info[3][2], board_info[3][3], board_info[3][4]])
+    game_board.add_row(["E", board_info[4][0], board_info[4][1], board_info[4][2], board_info[4][3], board_info[4][4]])
     print(game_board)
 
 
@@ -88,17 +84,17 @@ def place_ship_random(size):
                 ship.orientation = 3
 
 
-print("Welcome to Battleship! There is one ship that is", ship.length + 1, "unit(s) long.\nThe board is a 5 x 5 grid. "
-                                                                           "You will get", number_turns, "guesses to "
-                                                                                                         "find the "
-                                                                                                         "ship. Good "
-                                                                                                         "luck!")
+print("welcome to battleship! there is one ship that is", ship.length, "unit(s) long.\nthe board is a 5 x 5 grid. "
+                                                                       "you will get", number_turns, "guesses to "
+                                                                                                     "find the "
+                                                                                                     "ship. good "
+                                                                                                     "luck!")
 
 print_board(1)  # initiating game board
-place_ship_random(1)
+place_ship_random(ship.length)
 
-print(Fore.RED + "DEBUG:\nShip Location:\nco-ord:", ship.coord.x, ship.coord.y, "\nlength:", ship.length,
-      "\norientation:", ship.orientation, Fore.RESET)
+print(Fore.RED + "debug:\nship location:\nhuman co-ord:", number_to_letter[ship.coord.y], ship.coord.x + 1, "\nlength:",
+      ship.length, "\norientation:", ship.orientation, Fore.RESET)
 # Start the game play. If  you want to add delays, so the game plays more naturally use time.sleep(seconds)
 
 # We need to randomly place the ship on the board. The x-coordinate should have value between 0 and 4. If this were a
@@ -110,16 +106,14 @@ print(Fore.RED + "DEBUG:\nShip Location:\nco-ord:", ship.coord.x, ship.coord.y, 
 # version you build.
 for current_turn in range(number_turns):
     guess = Coordinate()
-    print("You are on turn ", current_turn + 1, "of", number_turns)
-    guess_string = input("Input guess: (ex. A1) ")
-    if len(guess_string) != 2:
-        print("The guess is not valid!")
+    print("you are on turn", current_turn + 1, "of", number_turns)
+    guess_string = input("input guess: (ex. A1) ")
+    if len(guess_string) != 2 or letter_to_number.get(guess_string[0].lower(), "INVALID") == "INVALID":
+        print("invalid input!")
+        guess.x, guess.y = -1, -1
     else:
-        guess.y = lettertrans.get(guess_string[0].lower(), "INVALID")
-        if guess.y == "INVALID":
-            print("invalid input!")
-            break
         guess.x = int(guess_string[1]) - 1
+        guess.y = letter_to_number.get(guess_string[0].lower())
     if (ship.orientation == 0 and guess.x == ship.coord.x and guess.y in range(ship.coord.y - (ship.length - 1),
                                                                                ship.coord.y + 1)) or (
             ship.orientation == 1 and guess.x in range(ship.coord.x,
@@ -128,25 +122,40 @@ for current_turn in range(number_turns):
                                                                                    ship.coord.y + 1)) or (
             ship.orientation == 3 and guess.x in range(ship.coord.x - (ship.length - 1),
                                                        ship.coord.x + 1) and guess.y == ship.coord.y):
-        # guess.x in range(ship.coord.x, ship.coord.x + ship.length - 1) and guess.y in range(ship.coord.y, ship.coord.y
-        # + ship.length - 1):
         board_info[guess.y][guess.x] = Fore.RED + "X" + Fore.RESET  # mark hit
+        number_hits += 1
         print_board()
-        print(Fore.RED + "Hit!\nYou hit my ship at (" + str(guess.x + 1) + ", " + str(guess.y + 1) + ")." + Fore.RESET)
+        print(Fore.RED + "hit!\nyou hit my ship at (" + str(guess.x + 1) + ", " + str(guess.y + 1) + ")." + Fore.RESET)
+        if number_hits == ship.length:
+            print("the ship sunk!\n"
+                  "congratulations, you won the game!")
+            break
     else:
         if guess.y not in range(len(board_info)) or guess.x not in range(len(board_info[0])):  # not on board
-            print("You missed the ocean!")
-        elif board_info[guess.y][guess.x] == "X":  # already guessed
-            print("You already guessed that location!")
+            print("you missed the ocean!")
+        elif board_info[guess.y][guess.x] in ("X", Fore.RED + "X" + Fore.RESET):  # already guessed
+            print("you already guessed that location!")
         else:  # on board but not guessed yet
-            print("You missed my battleship!")
+            print("you missed my battleship!")
             board_info[guess.y][guess.x] = "X"
-        if current_turn == number_turns - 1:
-            print("You have ran out of turns. The game is over!")
-            print("The ship was at row %d and column %d" % ship.coord.y, ship.coord.x)
-            # later change it to show the board with all ships highlighted
         print_board()
+        print("you have one ship remaining. it has a length of %d." % ship.length)
 
-        # Your print statement should should update how many ships are remaining and how many are sunk from your set of
-        # ships.
-        print("You have one ship remaining. It is a 1 x 1 unit.")
+print("you have ran out of turns. the game is over!")
+i = 0
+while i < ship.length:
+    if ship.orientation == 0:
+        if board_info[ship.coord.y - i][ship.coord.x] == "O":
+            board_info[ship.coord.y - i][ship.coord.x] = Fore.GREEN + "X" + Fore.RESET
+    if ship.orientation == 1:
+        if board_info[ship.coord.y][ship.coord.x + i] == "O":
+            board_info[ship.coord.y][ship.coord.x + i] = Fore.GREEN + "X" + Fore.RESET
+    if ship.orientation == 2:
+        if board_info[ship.coord.y + i][ship.coord.x] == "O":
+            board_info[ship.coord.y + i][ship.coord.x] = Fore.GREEN + "X" + Fore.RESET
+    if ship.orientation == 3:
+        if board_info[ship.coord.y][ship.coord.x - i] == "O":
+            board_info[ship.coord.y][ship.coord.x - i] = Fore.GREEN + "X" + Fore.RESET
+    i += 1
+print_board()
+print("ship locations are highlighted in green x.")
